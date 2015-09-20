0a7d7350ac777e83c58adabb361b0a6b9b7ee651
AlexFine <alexkfine2@gmail.com>
2015-09-19 21:56:27 -0700

    Some maestro code, will work if maestro goes back online. And UI improvements.

---
+++ myApp/www/js/script.js
@@ -0,0 +1,3 @@
+//alert("DISCLAIMER: This bot was created to enable you to anonymously let your friends know things, like their fly being open, etc." +
+//"What you do with this bot is up to you, we are not responsible for any improper usages.");
+var maestro = new Maestro("js/maestro_code.js");
